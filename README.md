487d757c38da21c661eefb2062f96425879964b2
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-06-16 22:31:41 +0300

    Stages 4 and 5 completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todo
 Simple to-do app on Golang
+
+## used:
+
+#### github.com/spf13/viper
+#### github.com/gin-gonic/gin
+#### github.com/lib/pq
+#### github.com/jmoiron/sqlx
+#### github.com/joho/godotenv
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 	"todo"
 	"todo/pkg/handler"
 	"todo/pkg/repository"
@@ -9,13 +13,39 @@ import (
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error occured while initializing config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to inizialization db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
-	
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
