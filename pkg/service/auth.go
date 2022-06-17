53c896235ca2436680c8ddaf0510b50de47847bb
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-06-17 23:44:34 +0300

    Stage 6 completed

---
+++ pkg/service/auth.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"todo"
+	"todo/pkg/repository"
+)
+
+const salt = "russianwarshipgofuckyourself"
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = s.generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
