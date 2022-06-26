6cfce9c3f617dda45cdfe7b504d4ae634d131da5
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-06-26 12:17:32 +0300

    Stage 10. Deleting lists

--- pkg/service/todo_list.go
+++ pkg/service/todo_list.go
@@ -24,3 +24,8 @@ func (s *TodoListService) GetAll(userId int) ([]todo.TodoList, error) {
 func (s *TodoListService) GetById(userId, listId int) (todo.TodoList, error) {
 	return s.repo.GetById(userId, listId)
 }
+
+func (s *TodoListService) Delete(userId, listId int) error {
+	return s.repo.Delete(userId, listId)
+
+}
