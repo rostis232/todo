065fbd1011f61d71d564f3bd07ac8f803e013faa
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-07-06 19:57:16 +0300

    Add for Items GetById

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -36,10 +36,13 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			{
 				items.POST("/", h.createItem)
 				items.GET("/", h.getAllItems)
-				items.GET("/:id", h.getItemById)
-				items.PUT("/:id", h.updateItem)
-				items.DELETE("/:id", h.deleteItem)
 			}
+		}
+		items := api.Group("items")
+		{
+			items.GET("/:id", h.getItemById)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
 		}
 	}
 	return router
--- pkg/service/todo_item.go
+++ pkg/service/todo_item.go
@@ -27,3 +27,7 @@ func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, e
 func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error){
 	return s.repo.GetAll(userId, listId)
 }
+
+func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
+	return s.repo.GetById(userId, itemId)
+}
